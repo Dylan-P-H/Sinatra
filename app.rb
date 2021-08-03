57f6308fcadaa29a63cb6f04ed39ddba1c67d2ed
Dylan <Dylan@eequ.org>
2021-08-03 14:46:03 +0100

    added a cat image

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
   "hello!"
@@ -6,4 +7,10 @@ end
 
 get '/secret' do
   "This is a secret"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://placekitten.com/500/500'>
+   </div>"
 end
